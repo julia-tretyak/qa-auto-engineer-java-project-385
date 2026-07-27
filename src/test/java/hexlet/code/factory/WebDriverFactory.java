e80170ead6fd8df3f70913d369832c5910eee391
Юлия Третьяк <julia@tretyak.com>
2026-07-27 13:13:28 +0300

    Simplify to exact config --no-sandbox --headless

--- src/test/java/hexlet/code/factory/WebDriverFactory.java
+++ src/test/java/hexlet/code/factory/WebDriverFactory.java
@@ -1,9 +1,5 @@
 package hexlet.code.factory;
 
-import hexlet.code.config.CiConfig;
-import hexlet.code.config.LocalConfig;
-import hexlet.code.config.TestConfig;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,42 +14,17 @@ public class WebDriverFactory {
     private static final Logger log = LoggerFactory.getLogger(WebDriverFactory.class);
 
     public static WebDriver create() {
-        TestConfig config = resolveConfig();
-        log.info("Creating WebDriver with config: headless={}", config.isHeadless());
-
-        WebDriverManager.chromedriver().setup();
+        log.info("Creating WebDriver");
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--remote-allow-origins=*");
-
-        if (config.isHeadless()) {
-            options.addArguments("--headless=new");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--window-size=1920,1080");
-        }
+        options.addArguments("--no-sandbox", "--headless");
 
         WebDriver driver = new ChromeDriver(options);
-
-        if (config.isHeadless()) {
-            driver.manage().window().setSize(new Dimension(1920, 1080));
-        } else {
-            driver.manage().window().maximize();
-        }
-
+        driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 
         log.info("WebDriver created successfully");
         return driver;
-    }
-
-    private static TestConfig resolveConfig() {
-        // В CI всегда задан APP_BASE_URL
-        if (System.getenv("APP_BASE_URL") != null) {
-            return new CiConfig();
-        }
-        return new LocalConfig();
     }
 }
